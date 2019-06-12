de0bd4f3766be5bf6f96a29b51e0b20953d4ad64
newb1e <ovst91@gmail.com>
2019-06-12 16:35:08 +0300

    simple working service

--- count-posts.py
+++ count-posts.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, abort, request
+from multiprocessing import Value
 
+counter = Value('i', 0)
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello():
-    counter = 0
-    return "Number of POST's is: "+str(counter)
+    if request.method == "POST":
+        with counter.get_lock():
+            counter.value += 1
+    elif request.method == "GET":
+        return "Number of POST's is: "+str(counter.value)
+    return "POST request"
+app.run(processes=8)
