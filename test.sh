76c762ae64d91d19c12853a6f9369ee1ea456827
newb1e <ovst91@gmail.com>
2019-06-16 20:45:56 +0300

    test123 in file count-posts.py

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 echo "Hello 1"
 docker build -t test1 https://github.com/newb1e/simple-devops-project.git
-docker run --rm -t -p 80:80 --name "test1" test1
+docker run --rm -ti -p 80:80 --name "test1" test1
